1c7a5f39eb561c5aab103f91c695d74a2e1ec6a2
altor <arthur.dazemar@free.fr>
2016-02-09 09:49:15 +0100

    ajout du module Window

---
+++ src/window.ml
@@ -0,0 +1,39 @@
+type window = int * int * string
+
+let make i j s = (i,j,s)
+
+let get_first_i window =
+  let i,_,_ = window
+  in i
+
+let get_second_i window =
+  let _,i,_ = window
+  in i
+
+let get_string window =
+  let _,_,s = window
+  in s
+			    
+let extract_window i length string =
+  let string_length = String.length string in
+  if i >= string_length
+  then None
+  else
+    let j =
+      match i with
+      | i when (i + length) >= string_length -> string_length - i
+      | i -> length
+    in let window_string = String.sub string i j
+       in Some (make i (i + j) window_string)
+	   
+let divise_chaine string length shift =
+  let rec aux list i = 
+    match extract_window i length string with
+      None -> list
+     | Some window -> aux (window::list) (i + shift)
+  in List.rev (aux [] 0)
+
+let rec extract_from_fasta longueur shift fasta =
+  match fasta with
+  | [] -> []
+  | (a,b)::suite ->  (a, divise_chaine b longueur shift) :: (extract_windows longueur shift suite)
---
+++ src/window.mli
@@ -0,0 +1,13 @@
+(** rerpésentation d'une fenêtre : (coordonée de la 1ere nucléotide, coordonée de la 2éme nucléotide, chaine de nucléotide) *)
+type window = int * int * string
+
+(** [extract_from_fasta l s fichier] renvoi la liste de toutes les fenêtres de taille [l] en commençant par le premier nucléotide et en se déplaçant de [s] nucléotides à chaque fenêtre*)
+val extract_from_fasta : int -> int -> Fasta.fasta -> (string * window list) list
+
+val make : int -> int -> string
+
+val get_first_i : window -> int
+
+val get_second_i : window -> int
+
+val get_string : window -> string
