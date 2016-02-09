31c0ca4e5262d988d62da61ad0ceed9c9b835afd
altor <arthur.dazemar@free.fr>
2016-02-09 13:25:56 +0100

    debug

--- src/k_mer.mli
+++ src/k_mer.mli
@@ -10,7 +10,7 @@ val list_of_window : int -> Window.window -> k_mer list
 val list_spaced : string -> Fasta.fasta -> k_mer list
 
 (** Comme [list_spaced] mais utilise une window au lieu d'un fasta *)
-val list_spaced_of_window : string -> window -> k_mer list
+val list_spaced_of_window : string -> Window.window -> k_mer list
 						 
 (** [common l1 l2] renvoie la liste des K_mers de la liste [l1] présents dans la liste [l2]. Sa complexité est de [O(n * log(n))] avec [n], le nombre de nucléotide présentes dans la structure [fasta]*)
 val common : k_mer list -> k_mer list -> k_mer list
