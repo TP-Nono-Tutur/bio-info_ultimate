e10b72bca53ad0a9906bcd1a7e8875c1223907ed
altor <arthur.dazemar@etudiant.univ-lille1.fr>
2017-10-26 11:38:12 +0200

    add compression function

--- src/compress.mli
+++ src/compress.mli
@@ -4,4 +4,5 @@ type compressed_fasta : bytes
 (** [to_normal_file file_name  fasta ]ecris l'ensemble des sequence de [fasta] dans le fichier [file_name]*)
 val to_normal_file : out_channel -> Fasta.t ->  unit
 
-val compress : Fasta.t
+val compress : Fasta.t -> out_channel -> unit
+val decompress : in_channel -> out_channel -> unit
